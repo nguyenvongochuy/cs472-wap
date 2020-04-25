9425f9291848864a32884277963fb49fd3be05c7
nguyenvongochuy <nguyenvongochuy@gmail.com>
2020-04-25 11:07:56 -0500

    updated clock js

--- w1d5/js/clock.js
+++ w1d5/js/clock.js
@@ -1,15 +1,22 @@
-function currentTime() {
-  var date = new Date(); /* creating object of Date class */
+/**
+ * Display current time function and Immediately-Invoked Function
+ */
+(function currentTime() {
+  var date = new Date(); 
   var hour = date.getHours();
   var min = date.getMinutes();
   var sec = date.getSeconds();
   hour = updateTime(hour);
   min = updateTime(min);
   sec = updateTime(sec);
+  
   document.getElementById("clock").innerText = hour + " : " + min + " : " + sec; /* adding time to the div */
   var t = setTimeout(function(){ currentTime() }, 1000); /* setting timer */
-}
+}());
 
+/**
+ * Add first 0 to have 2 digit if only have 1 digit
+ */
 function updateTime(k) {
   if (k < 10) {
     return "0" + k;
@@ -19,4 +26,3 @@ function updateTime(k) {
   }
 }
 
-currentTime();
